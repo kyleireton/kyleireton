73f5826c4d2ce26baf3466053896d7e8fbaec203
eugenin1 <eugenin1@businesspartner.roche.com>
2023-02-03 15:52:15 +0000

    Initial commit

---
+++ Connecting to Github.R
@@ -0,0 +1,20 @@
+# Connect existing rstudio cloud environment to github using R
+
+install.packages("usethis")
+# Add authentication information: Use your GitHub user name and associated email address.
+usethis::use_git_config(user.name = "eugenin1",
+                        user.email = "eugenin1@businesspartner.roche.com")
+
+# Initiate git:
+usethis::use_git()
+
+# Create GH-Token:
+usethis::create_github_token()
+# This takes you to github. Choose settings, create GH-Token (store it in a password manager).
+
+# Now use:
+gitcreds::gitcreds_set()
+# And enter the created GH-Token in the R console when prompted
+
+# Create GitHub repo using:
+usethis::use_github()
