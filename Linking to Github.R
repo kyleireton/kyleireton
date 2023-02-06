41bae3c4eca7ad2574043779ed2f333a0f603e6a
nicholaswe <nicholaswe@gmail.com>
2023-02-06 14:18:39 +0000

    Updates about Token

--- Linking to Github.R
+++ Linking to Github.R
@@ -5,6 +5,12 @@ install.packages("usethis") # Do it only in the first time
 usethis::use_git_config(user.name = "nicholaswe",
                         user.email = "nicholaswe@gmail.com")
 
-# Token: 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
+# Each person needs to create its own Token (do it only in the first time)
+
+usethis::create_github_token()
+
+# Insert the created token
 
 gitcreds::gitcreds_set()
+
+
